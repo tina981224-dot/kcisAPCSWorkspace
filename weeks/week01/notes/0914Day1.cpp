7e7a3f55f1ee22acb3f3b02882e01a4f0574952d
123 <tina981224@gmail.com>
2026-09-21 18:08:24 +0800

    啊

---
+++ weeks/week01/notes/0914Day1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << "Device:" << a << endl;
+    cout << "Channel:" << b << endl;
+
+    return 0;
+}
---
+++ weeks/week04/problems/basic/01-pairPackaging/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n = 0;
+    cin>>n;
+    if(n%2==0){
+        cout<<1;
+    }else cout>>0;
+    return 0;
+}
